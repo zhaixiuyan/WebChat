ac703c9fb02de28a58766c324160e5f2afe1a180
zhaixiuyan <xy_zhai@163.com>
2020-04-30 15:37:00 +0800

    增加用戶認證

--- app01/routing.py
+++ app01/routing.py
@@ -4,7 +4,7 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
 from app01 import consumers
-
+from channels.auth import AuthMiddlewareStack
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
         url(r'^chat/$', consumers.ChatConsumer),
